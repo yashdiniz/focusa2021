d95b8095052035c4a629a8e15d6d4ebbd18ad02f
Yash Diniz <yashdiniz@gmail.com>
2020-11-22 19:09:20 +0530

    Learning a lot!

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,17 +1,79 @@
 const graphql = require('graphql');
-const { GraphQLID, GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { 
+    GraphQLID, 
+    GraphQLObjectType, 
+    GraphQLString, 
+    GraphQLSchema, 
+    GraphQLList, 
+    GraphQLInt 
+} = graphql;
+
+const booksTable = [ // temporarily using a simple JSON lookup table
+    {
+        id: 'jkrhp1',
+        name: "Harry Potter and the Philosopher's stone.",
+        genre: "Fiction, Mystery",
+        author: 'jkr',
+    },
+    {
+        id: 'jkrhp2',
+        name: "Harry Potter and the Chamber of Secrets.",
+        genre: "Fiction, Mystery",
+        author: 'jkr',
+    },
+    {
+        id: 'cdpp',
+        name: "The Pickwick Papers.",
+        genre: "Fiction",
+        author: 'cd',
+    },
+    {
+        id: 'hgwim',
+        name: "The Invisible Man.",
+        genre: "Science Fiction",
+        author: 'hgw',
+    },
+]
+const authorsTable = [
+    { id:'jkr', name: "JK Rowling", age: 44 },
+    { id:'cd', name: "Charles Dickens", age: 256 },
+    { id:'hgw', name: "HG Wells", age: 123 },
+]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
-    description: "Book Type: Holds book details",
+    description: "Holds book details",
     fields: () => ({ // using lazy evaluation to help fix cyclical references
         id : {
             type: GraphQLID
         },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args, ctx, info) {  // We put resolve in BookType, not in AuthorType, because resolution should happen at reference
+                // `parent` holds what was resolved at the higher node.
+                return authorsTable.find(e => e['id'] == parent['author'])
+            }
+        },
     })
-})
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    description: "Holds author details",
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        books: {
+            type: GraphQLList(BookType),
+            resolve(parent) {
+                return booksTable.filter(e => e['author'] == parent['id'])  // filters out all unnecessary
+            }
+        }
+    })
+});
 
 const RootQueryType = new GraphQLObjectType({
     name: 'RootQuery',
@@ -19,12 +81,36 @@ const RootQueryType = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            desription: "Get a book based on the particular ID.",
+            description: "Get a book based on ID.",
             args: {
                 id: { type: GraphQLID }
             },
             resolve (parent, args, ctx, info) {
-                // code to resolve data from DB
+                return booksTable.find(e => e['id'] == args.id);
+            }
+        },
+        books: {
+            type: GraphQLList(BookType),
+            description: "Get all books.",
+            resolve () {
+                return booksTable
+            }
+        },
+        author: {
+            type: AuthorType,
+            description: "Get an author based on ID.",
+            args: {
+                id: { type: GraphQLID },
+            },
+            resolve(_, args) {
+                return authorsTable.find(e => e['id'] == args.id);
+            }
+        },
+        authors: {
+            type: GraphQLList(AuthorType),
+            description: "Get all authors",
+            resolve () {
+                return authorsTable;
             }
         }
     }
