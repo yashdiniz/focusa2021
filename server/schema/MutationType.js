6f5e854c6e339e7ee8e70207c71e71a1965c37d1
Yash Diniz <yashdiniz@gmail.com>
2020-11-23 13:12:14 +0530

    Description string now simplified to a single line

--- server/schema/MutationType.js
+++ server/schema/MutationType.js
@@ -7,9 +7,7 @@ const { GraphQLObjectType } = require('graphql');
 
 const MutationType = new GraphQLObjectType({
     name: 'Mutation',
-    description: "This node serves as a mutable entrypoint to the graph. \
-    It contains fields which we can use for traversing through the graph \
-    and performing write, update and delete operations.",
+    description: "This node serves as a mutable entrypoint to the graph. It contains fields which we can use for traversing through the graph and performing write, update and delete operations.",
     fields: {
         
     }
--- server/schema/QueryType.js
+++ server/schema/QueryType.js
@@ -7,9 +7,7 @@ const { GraphQLObjectType } = require('graphql');
 
 const QueryType = new GraphQLObjectType({
     name: 'Query',
-    description: "This node serves as an immutable entrypoint to the graph. \
-    It contains fields which we can use for traversing through the graph \
-    and obtaining information.",
+    description: "This node serves as an immutable entrypoint to the graph. It contains fields which we can use for traversing through the graph and obtaining information.",
     fields: {
 
     }
