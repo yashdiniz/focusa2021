26eaae1a5da0d603bc6b401489efd0ff22540b7f
Yash Diniz <yashdiniz@gmail.com>
2020-11-23 15:08:01 +0530

    Added a stub for ProfileType.

---
+++ server/schema/ProfileType.js
@@ -0,0 +1,52 @@
+/**
+ * The Profile node holds additional information,
+ * like the display picture, full name, about, and interests.
+ * The profile is isolated from the User for shorter queries.
+ */
+const {
+    GraphQLObjectType,
+    GraphQLList,
+    GraphQLID,
+    GraphQLString,
+    GraphQLNonNull,
+} = require('graphql');
+const UserType = require('./UserType');
+const CourseType = require('./CourseType');
+
+const ProfileType = new GraphQLObjectType({
+    name: 'Profile',
+    description: "This node holds additional information related to a User.",
+    fields: {
+        id: {
+            type: GraphQLNonNull(GraphQLID),
+        },
+        user: {
+            type: GraphQLNonNull(UserType),
+            description: "User that maps to this profile.",
+            resolve(parent, args, ctx, info) {
+                // currently stub, return null
+            }
+        },
+        fullName: {
+            type: GraphQLNonNull(GraphQLString),
+            description: "The full name of the profile.",
+        },
+        about: {
+            type: GraphQLNonNull(GraphQLString),
+            description: "The about paragraph of the profile.",
+        },
+        display_pic: {
+            type: GraphQLNonNull(GraphQLString),
+            description: "URL to the display picture of the profile.",
+        },
+        interests: {
+            type: GraphQLList(CourseType),
+            description: "The list of courses the user has subscribed to.",
+            resolve(parent, args, ctx, info) {
+                // currently stub, return null
+            }
+        }
+    }
+});
+
+module.exports = ProfileType;
