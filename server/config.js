e8563825fc9cf5bb5234f7a46df844fabcc56762
Yash Diniz <yashdiniz@gmail.com>
2020-12-29 13:45:02 +0530

    Auth config secret added.

--- server/config.js
+++ server/config.js
@@ -1,4 +1,8 @@
 const port = 1896;   // using FOCUSA legacy port for testing.
 const graphiql = true;  // essentially, run graphiql at graphql endpoint
 
-module.exports = { port, graphiql };
+// the dev secret which will be used for most crypto operations.
+// IN PRODUCTION, it should be `process.env.secret` with an environment variable.
+const secret = "b-3l][=08BOIHW[oH)#T)(HG{3-09g2p[i-u";
+
+module.exports = { port, graphiql, secret };
