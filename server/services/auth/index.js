4d78c7641ea650b4bb6cdff2e72874ad71a9c5af
Yash Diniz <yashdiniz@gmail.com>
2020-12-30 11:06:57 +0530

    Imported packages needed for auth

--- server/services/auth/index.js
+++ server/services/auth/index.js
@@ -1,2 +1,9 @@
 const express = require('express');
-const app = express();
+const router = express.Router();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+router.get('/login', 
+
+)
