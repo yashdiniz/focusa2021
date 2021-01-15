923fb3d12d033c14f22f9ea8e254335c085c6e34
Yash Diniz <yashdiniz@gmail.com>
2021-01-15 23:10:28 +0530

    Made graphiql appear again.

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const port = process.env['PORT'];   // using FOCUSA legacy port for testing.
-const graphiql = !process.env['PRODUCTION'];  // essentially, run graphiql at graphql endpoint
+const graphiql = process.env['PRODUCTION'] == 'false';  // essentially, run graphiql at graphql endpoint
 
 // the dev secret which will be used for most crypto operations.
 const secret = process.env['SECRET'];
