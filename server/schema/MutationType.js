1da9e632df252649e2283c23affc3e6cea2345e6
Yash Diniz <yashdiniz@gmail.com>
2021-02-19 17:26:59 +0530

    Stub comments of features which need to be coded.

--- server/schema/MutationType.js
+++ server/schema/MutationType.js
@@ -9,7 +9,8 @@ const MutationType = new GraphQLObjectType({
     name: 'Mutation',
     description: "This node serves as a mutable entrypoint to the graph. It contains fields which we can use for traversing through the graph and performing write, update and delete operations.",
     fields: {
-        
+        // publishPost(input MakePost)
+        // adding mutations will be subject to frontend requirements for now.
     }
 })
 
--- server/schema/QueryType.js
+++ server/schema/QueryType.js
@@ -9,7 +9,12 @@ const QueryType = new GraphQLObjectType({
     name: 'Query',
     description: "This node serves as an immutable entrypoint to the graph. It contains fields which we can use for traversing through the graph and obtaining information.",
     fields: {
-
+        // getProfile(ID)
+        // getUser(ID)
+        // getCourse(ID)
+        // searchCourses(string search query)
+        // getPost(ID)
+        // searchPosts(string search query)
     }
 })
 
