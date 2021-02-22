acbf8e393f177d4ad63fe311ef28f044ae0a3e26
Alston Dias <nirbhey.015@cloudcounselage.com>
2021-02-22 12:16:14 +0530

    added map-reduce index

--- server/services/courses/db.js
+++ server/services/courses/db.js
@@ -1,2 +1,26 @@
-var PouchDB = require("pouchdb");
-var courses = new PouchDB("")
+const { logs, courses, assert } = require('../databases');
+
+var PouchDB = require("pouchdb");   
+
+function addcourse(name, description, mods){
+
+
+}
+
+// document that tells PouchDB/CouchDB
+// to build up an index on doc.name
+var ddoc = {
+    _id: '_design/my_index',
+    views: {
+      by_name: {
+        map: function (doc) { emit(doc.name); }.toString(),
+        reduce: '_count'
+      }
+    }
+  };
+  // save it
+  courses.put(ddoc).then(function () {
+    // success!s
+  }).catch(function (err) {
+    // some error (maybe a 409, because it already exists?)
+  });
