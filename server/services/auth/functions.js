35930f601eb72bcbf8bfd9cc1b476574769601b5
Yash Diniz <yashdiniz@gmail.com>
2021-02-22 08:15:41 +0530

    Adding basic db and security functionality.

--- server/services/auth/functions.js
+++ server/services/auth/functions.js
@@ -8,16 +8,16 @@
  * 
  * author: @yashdiniz
  */
-const path = require("path") , fs = require("fs");
-
-process.title = "FOCUSA auth service";
+process.title = 'FOCUSA auth service';
+const { logs, auth, assert, generateUUID } = require('../databases');
+const { pbkdfDigest, pbkdfIters, pbkdfLen, usernamePattern, currentPasswordScheme } = require('../../config');
+const crypto = require('crypto');
 
 // Reference: https://pouchdb.com/2015/05/18/we-have-a-problem-with-promises.html
-pbkdf = (word, salt) => new Promise((resolve, reject) => 	// will return a Promise!
-		crypto.pbkdf2(word, salt, config.pbkdfIters, config.pbkdfLen, config.pbkdfDigest,
+const pbkdf = (word, salt) => new Promise((resolve, reject) => 	// will return a Promise!
+		crypto.pbkdf2(word, salt, pbkdfIters, pbkdfLen, pbkdfDigest,
 		(err, key) => err !== null ? reject(err) : resolve(key.toString('base64'))));
 
-// TODO: reference logs to a local DB, and synchronise!!
 // logs.get('_local/firstTime')	// check the presence of the flag
 // .catch(e=>{						// if it does not exist
 // 	console.log("Welcome to FOCUSA! Initialising database.");
@@ -29,9 +29,39 @@ pbkdf = (word, salt) => new Promise((resolve, reject) => 	// will return a Promi
 // 	}));
 // }).catch(e=> console.error("Database initialization failed.", e));
 
-// assert(condition, message) simplifies the code structure, by immediately throwing if condition is not met.
-let assert = (condition, message) => {
-    if(!condition) throw message || "Assertion failed!";
-    else return true;
-};
+// Use this to enforce one form of low-risk-low-conflict uniqueness.
+// Reference: https://stackoverflow.com/a/22440576/13227113
+auth.createIndex({  // creating an index for enforcing uniqueness in usernames
+	index: { 
+		fields: ['name'],
+		ddoc: 'indexes',
+		name: 'auth_unique'
+	}
+}).then(() => auth.find({
+	selector: { 
+		name: null 
+	}, 
+	use_index:'indexes', 
+	limit: 0 
+})).catch(e => console.error(e));
 
+const createUser = (name, password) => {
+    assert(typeof name === 'string' && typeof password === 'string',
+        "Invalid arguments for createUser.");
+    assert(name == name.match(usernamePattern), "User not created. name should match: " + usernamePattern);
+    let salt = generateUUID();
+    return pbkdf(password, salt)    // hash the password
+    .then(hash => {
+        auth.post({ // db.post() ensures random UUIDs, useful in this scenario
+            name, hash, salt, scheme: currentPasswordScheme
+        }).then(
+            // 1. temp hold the ID of document just posted.
+            // 2. try to find(using auth_unique index) for another document with matching name.
+            // 3. if a match is found, then delete the document just posted, and throw failure.
+        )
+        return auth.find({
+            selector: { name }, // search for any users already with this name...
+            use_index:'indexes',
+        });
+    })
+}
--- server/services/databases.js
+++ server/services/databases.js
@@ -3,8 +3,10 @@
  * along with push-pull replication handlers.
  * author: @yashdiniz
  */
-const PouchDB = require("pouchdb");
-PouchDB.plugin(require("pouchdb-find"));
+const path = require('path'), crypto = require('crypto');
+const { UUIDSize } = require('../config');
+const PouchDB = require('pouchdb');
+PouchDB.plugin(require('pouchdb-find'));
 
 const dbDirectory = 'db',   // the directory where DB is stored locally
     remote = 'http://admin:admin@localhost:5984/';	// the remote database to sync with!
@@ -24,15 +26,27 @@ const syncHandlers = [
 const sync = () => {
     syncHandlers.forEach(o => {
         o.handler.on('complete', 
-            async ()=> console.log("Database", o.db, "synced.")
+            async ()=> console.log('Database', o.db, 'synced.')
         ).on('error', e=>console.log(e));
     });
 };
+
+// assert(condition, message) simplifies the code structure, by immediately throwing if condition is not met.
+const assert = (condition, message) => {
+    if(!condition) throw message || 'Assertion failed!';
+    else return true;
+};
+
+// generate a random number as UUID
+// the UUID is made URL safe, while still maintaining base64, by basic substitution
+const generateUUID = (size) => crypto.rng(size || UUIDSize)
+                    .toString('base64')
+                    .replace(/\//gi,'.').replace(/\+/gi,'-').replace(/\=/gi,'_')
 
 // initiate the replication
 sync();
 
 module.exports = {
     logs, auth, roles,
-
+    assert, generateUUID
 }
