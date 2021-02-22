7a2fea83b31fe83f9a40b13ead57af1efe46e61a
Alston Dias <nirbhey.015@cloudcounselage.com>
2021-02-22 16:53:22 +0530

    Switched to RxDB.

--- server/services/databases.js
+++ server/services/databases.js
@@ -5,31 +5,16 @@
  */
 const path = require('path'), crypto = require('crypto');
 const { UUIDSize, maxModRolesforCourse, defaultProfilePic } = require('../config');
-const PouchDB = require('pouchdb');
-PouchDB.plugin(require('pouchdb-find'));
 
-const dbDirectory = 'db',   // the directory where DB is stored locally
-    remote = 'http://admin:admin@localhost:5984/focusa';	// the remote database to sync with!
+const remote = 'http://admin:admin@localhost:5984/focusa';	// the remote database to sync with!
 
-// the list of local PouchDB databases
-const logs = new PouchDB(path.join(dbDirectory, 'logs'));
-const roles = new PouchDB(path.join(dbDirectory, 'roles'));
-const auth = new PouchDB(path.join(dbDirectory, 'auth'));
-
-// initiating push-pull replication with the remote CouchDB instance.
-const syncHandlers = [
-	{db:'logs', handler:logs.sync(new PouchDB(remote + 'logs'), {live: true})},
-	{db:'auth', handler:auth.sync(new PouchDB(remote + 'auth'), {live: true})},
-	{db:'roles', handler:roles.sync(new PouchDB(remote + 'roles'), {live: true})},
-];
-
-const sync = () => {
-    syncHandlers.forEach(o => {
-        o.handler.on('complete', 
-            async ()=> console.log('Database', o.db, 'synced.')
-        ).on('error', e=>console.log(e));
-    });
-};
+// const sync = () => {
+//     syncHandlers.forEach(o => {
+//         o.handler.on('complete', 
+//             async ()=> console.log('Database', o.db, 'synced.')
+//         ).on('error', e=>console.log(e));
+//     });
+// };
 
 // assert(condition, message) simplifies the code structure, by immediately throwing if condition is not met.
 const assert = (condition, message) => {
@@ -44,7 +29,7 @@ const generateUUID = (size) => crypto.rng(size || UUIDSize)
                     .replace(/\//gi,'.').replace(/\+/gi,'-').replace(/\=/gi,'_')
 
 // initiate the replication
-sync();
+// sync();
 
 const RxDB = require('rxdb');
 const leveldown = require('leveldown');
@@ -244,6 +229,6 @@ const replicationState = collections.sync({
 });
 
 module.exports = {
-    logs, auth, roles,
+    collections,
     assert, generateUUID
 }
