5029107d24ff0655fa1992582a4067fa86a2d7e8
Alston Dias <nirbhey.015@cloudcounselage.com>
2021-02-28 00:06:03 +0530

    Login page is getting ready

--- client/focusa2021/Activity/Login.js
+++ client/focusa2021/Activity/Login.js
@@ -1,12 +1,37 @@
 import React, { useState, Component } from 'react';
-import { Button, Text, View, SafeAreaView, ActivityIndicator, StyleSheet } from 'react-native';
+import { View,Text,StatusBar,Image, TextInput } from 'react-native';
+import styles from '../Styles/LoginStyles';
+import * as Font from 'expo-font';
+
+
 
 class Login extends Component{
+
+    async loadFonts() {
+        await Font.loadAsync({
+            ZCOOLKuaiLe: require('../assets/fonts/ZCOOLKuaiLe.ttf')})
+    }
+    UNSAFE_componentWillMount(){
+        this.loadFonts();
+    }
+    componentDidMount(){
+        this.loadFonts();
+    }
+    componentDidUpdate(){
+        this.loadFonts();
+    }
+
     render(){
         return(
-            <View>
-                
+            <View style={styles.container}>
+                <StatusBar backgroundColor = "#000000" barStyle="light-content"/> 
+                <Image style={styles.image} source={require('../assets/images/focusa2.png')}/>
+                <Text style={styles.focusa}>FOCUSA</Text>
+                <TextInput style={styles.inputBox} placeholder="Username"/>
+                <TextInput style={styles.inputBox} placeholder="Password" secureTextEntry={true}/>
             </View>
-        )
+            
+        );
     }
 }
+export default Login;
---
+++ client/focusa2021/Styles/LoginStyles.js
@@ -0,0 +1,30 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container:{
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    image:{
+        marginTop: 40,
+        width: 60,
+        height: 200,
+    },
+
+    focusa:{
+        marginTop: 30,
+        fontFamily: 'ZCOOLKuaiLe',
+        fontSize: 64
+    },
+
+    inputBox:{
+        height: 40,
+        width: 250,
+        borderColor: 'gray',
+        borderWidth: 3,
+        marginTop: 30,
+        padding: 10,
+        borderRadius: 17,
+    }
+})
