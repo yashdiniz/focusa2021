1fead6670e48a32db0d75c465a740930c6a04fc6
Yash Diniz <yashdiniz@gmail.com>
2021-03-02 17:00:45 +0530

    Added profile functions.

---
+++ server/services/profile/functions.js
@@ -0,0 +1,71 @@
+/**
+ * author: @reydias06
+ */
+
+const { focusa, assert } = require('../databases');
+
+const userNonExistant = new Error('User does not exist.');
+
+const createProfile = async (userID)=> {
+    assert(typeof userID === 'string',
+    'Invalid arguments for createProfile.');
+
+    let c = await focusa;
+    return await c.user.findOne(userID).exec()
+    .then(doc => {
+        if (doc) {
+            return c.profile.insert({
+                userID,
+                fullName: 'User Profile Name', 
+                about: 'Hey, I am a new User !',
+                interests: [],
+            });
+        } else throw userNonExistant;
+    });
+};
+
+const getProfile = async (userID)=> {
+    assert(typeof userID === 'string',
+    'Invalid arguments for getProfile.');
+
+    let c = await focusa;
+    return await c.user.findOne(userID).exec()
+    .then(doc => {
+        if (doc) {
+            return c.profile.findOne(userID).exec();
+        } else throw userNonExistant;
+    })
+}
+
+const updateProfile = async (userID, fullName, about, display_pic)=> {
+    assert(typeof userID === 'string' &&
+    typeof fullName === 'string' &&
+    typeof about === 'string' &&
+    (!display_pic ||  typeof display_pic === 'string'),
+    'Invalid arguments for createProfile.');
+
+    let c = await focusa;
+    return await c.user.findOne(userID).exec()
+    .then(async doc => {
+        if (doc) {
+            let profile = await c.profile.findOne(userID).exec();
+            profile.atomicPatch({
+                fullName,
+                about,
+                display_pic,
+            });
+        } else throw userNonExistant;
+    });
+};
+
+const addInterest = async () => {
+    // TODO
+};
+
+const removeInterest = async () => {
+    // TODO
+}
+
+module.exports = {
+    createProfile, getProfile, updateProfile
+}
