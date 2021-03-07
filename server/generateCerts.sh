1abee68a1b0f69c1719bd88a7782da1af14aa54d
Yash Diniz <yashdiniz@gmail.com>
2021-03-07 18:51:55 +0530

    Changed directory to certs folder

--- server/generateCerts.sh
+++ server/generateCerts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out certificate.pem
-openssl x509 -text -noout -in certificate.pem
-openssl pkcs12 -inkey key.pem -in certificate.pem -export -out certificate.p12 # gyroscope: subject to change
-openssl pkcs12 -in certificate.p12 -noout -info
+mkdir certs
+openssl req -newkey rsa:2048 -nodes -keyout certs/key.pem -x509 -days 365 -out certs/certificate.pem
+openssl x509 -text -noout -in certs/certificate.pem
+openssl pkcs12 -inkey certs/key.pem -in certs/certificate.pem -export -out certs/certificate.p12 # gyroscope: subject to change
+openssl pkcs12 -in certs/certificate.p12 -noout -info
 
