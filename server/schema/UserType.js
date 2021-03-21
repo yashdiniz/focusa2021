d7e80fc7269e2cf15e55b9d29a7e38403b0e89c9
Yash Diniz <yashdiniz@gmail.com>
2021-03-21 12:06:55 +0530

    Connected UserType to auth, also fixed cyclicals.

--- server/schema/UserType.js
+++ server/schema/UserType.js
@@ -10,14 +10,20 @@ const {
     GraphQLList,
     GraphQLNonNull,
 } = require('graphql');
-const ProfileType = require('./ProfileType');
-const PostType = require('./PostType');
-const RoleType = require('./RoleType');
+const { create } = require('axios');
+const { authRealm } = require('../config');
+
+const auth = create({
+    baseURL: `${authRealm}`,
+    timeout: 5000,
+});
 
 const UserType = new GraphQLObjectType({
     name: 'User',
     description: "This node holds all the necessary user details.",
-    fields: () => ({
+    fields: () => {
+        const { ProfileType, PostType, RoleType } = require('./types');
+        return {
         uuid: { 
             type: GraphQLNonNull(GraphQLID),
         },
@@ -25,28 +31,31 @@ const UserType = new GraphQLObjectType({
             type: GraphQLNonNull(GraphQLString),
             description: "A unique username.",
         },
-        // profile: { 
-        //     type: ProfileType,
-        //     description: "Profile that maps to this User.",
-        //     resolve(parent, args, ctx, info) {
-        //         // currently stub, return null
-        //     }
-        // },
-        // posts: {
-        //     type: GraphQLNonNull(GraphQLList(PostType)),
-        //     description: "Posts written by this Person.",
-        //     resolve(parent, args, ctx, info) {
-        //         // currently stub, return null
-        //     }
-        // },
-        // roles: {
-        //     type: GraphQLNonNull(GraphQLList(RoleType)),
-        //     description: "Roles that the User is assigned.",
-        //     resolve(parent, args, ctx, info) {
-        //         // currently stub, return null
-        //     }
-        // },
-    })
+        profile: { 
+            type: ProfileType,
+            description: "Profile that maps to this User.",
+            resolve(parent, args, ctx, info) {
+                // currently stub, return null
+            }
+        },
+        posts: {
+            type: GraphQLNonNull(GraphQLList(PostType)),
+            description: "Posts written by this Person.",
+            resolve(parent, args, ctx, info) {
+                // currently stub, return null
+            }
+        },
+        roles: {
+            type: GraphQLNonNull(GraphQLList(RoleType)),
+            description: "Roles that the User is assigned.",
+            async resolve(parent, args, ctx, info) {
+                return await auth.get('/getRolesOfUser', {
+                    params: { name: parent['name'] },
+                    headers: { authorization: ctx.headers.authorization },
+                }).then(res => res.data);
+            }
+        }}
+    }
 });
 
 module.exports = UserType;
