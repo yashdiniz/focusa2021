e692b05b14713cef66555ecdf9f9e36f6e972747
Yash Diniz <yashdiniz@gmail.com>
2021-03-21 20:05:45 +0530

    Made intro comment.

--- server/schema/types.js
+++ server/schema/types.js
@@ -1,8 +1,8 @@
-/**
+/*
  * Experimenting alleviation of cyclical dependencies.
  * Using dependency injection.
  * Reference: https://stackoverflow.com/a/62487603
- */
+*/
 const CourseType = require('./CourseType');
 const PostType = require('./PostType');
 const ProfileType = require('./ProfileType');
