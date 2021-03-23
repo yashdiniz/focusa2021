f8399a543720fcf102cb2603eba74cdb30261b0f
Imamsab <imamsab35@gmail.com>
2021-03-23 20:07:49 +0530

    added addCourse f and session handling

--- server/services/courses/functions.js
+++ server/services/courses/functions.js
@@ -6,10 +6,24 @@
  * incorporated into the final microservice,
  * added for house-keeping tasks.
  * 
- * author: @yashdiniz
+ * author: @imamsab
  */
 const { focusa, assert, generateUUID } = require('../databases');
-const { maxModRolesforCourse } = require('../../config');
+const { create } = require('axios');
+const { maxModRolesforCourse, authRealm, serviceAuthPass, JWTsignOptions } = require('../../config');
+
+let token = '';
+const auth = create({
+    baseURL: `${authRealm}`,
+    timeout: 5000,
+    headers: { 'authorization': token },
+});
+// TODO: add the base64 encode function
+// TODO: also take care of the refresh session scenario!
+let loginDetails = base64Encode(`courses:${serviceAuthPass}`);
+setInterval(() => auth.get('/', {
+    headers: `Basic ${loginDetails}`
+}).then(res => token = res.data.token), (JWTsignOptions.expiresIn-10)*1000);
 
 /**
  * Adds a course to the database.
@@ -17,9 +31,20 @@ const { maxModRolesforCourse } = require('../../config');
  * @param {string} description Description of the course (not a key).
  */
 const addCourse = async (name, description) => {
-    assert(typeof name === 'string' && typeof description === 'string',
-    "Invalid arguments for addCourse.");
-    
+    assert(typeof name === 'string' && typeof description === 'string');
+
+    let uuid = generateUUID();
+    let f = await focusa;
+
+    let admin = await auth.get('/getRoleByName', {
+        params: { name: 'admin' },
+        // headers: { Authorization:  }
+    }).then(res => res.data.uuid);
+
+    return await f.courses.insert({
+        uuid,name, description,
+        mods: [ admin ] // allowing only admin users to moderate
+    });
 };
 
 module.exports = {
