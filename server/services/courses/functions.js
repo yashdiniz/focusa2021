0221891236844a6940fcfe7449e63cd07fe34c4b
Alston Dias <nirbhey.015@cloudcounselage.com>
2021-03-24 16:46:47 +0530

    completed crud fuctions for courses

--- server/services/courses/functions.js
+++ server/services/courses/functions.js
@@ -11,6 +11,11 @@
 const { focusa, assert, generateUUID } = require('../databases');
 const { create } = require('axios');
 const { maxModRolesforCourse, authRealm, serviceAuthPass, JWTsignOptions } = require('../../config');
+const { name } = require('dayjs/locale/*');
+const e = require('express');
+
+const CourseExistsError = new Error('Course alredy exists'),
+      CourseNonExsistent = new Error('Course Does not exists');
 
 let token = '';
 const auth = create({
@@ -31,7 +36,7 @@ setInterval(() => auth.get('/', {
  * @param {string} description Description of the course (not a key).
  */
 const addCourse = async (name, description) => {
-    assert(typeof name === 'string' && typeof description === 'string');
+    assert(typeof name === 'string' && typeof description === 'string', 'Invalid arguments for addCourse.');
 
     let uuid = generateUUID();
     let f = await focusa;
@@ -44,9 +49,84 @@ const addCourse = async (name, description) => {
     return await f.courses.insert({
         uuid,name, description,
         mods: [ admin ] // allowing only admin users to moderate
-    });
+    }).catch(e => { throw CourseExistsError });
 };
 
+/**
+ * get a course with the matching id
+ * @param {string} id database key to identify the course
+ * @returns the  course object with matching id
+ */
+const getCourseById = async (id) => {
+    assert(typeof id === 'string', 'Invalid arguments for getCourseById.');
+    let c = await focusa;
+
+    return await c.courses.findOne(id).exec()
+    .then(async doc=>{
+        if (doc) return doc;
+
+        else throw CourseNonExsistent;
+    });
+
+}
+
+/**
+ * get course with the matching name
+ * @param {string} name name of the course to find
+ * @returns the course object with the matching name
+ */
+const getCourseByName = async(name) => {
+    assert(typeof name === 'string', 'Invalid arguments for getCourseByName');
+    let c = await focusa;
+
+    return await c.courses.find({selector:{name}}).exec()
+    .then(async doc=>{
+        if (doc) return doc;
+
+        else throw CourseNonExsistent;
+    });
+}
+
+/**
+ * update course with matching id
+ * @param {string} id id of the course to update
+ * @param {string} name name to update the course to
+ * @param {string} description description to update the course to
+ * @returns the updated name and description of the course
+ */
+const updateCourse = async(id,name,description) => {
+
+    assert(typeof id ==='string' && typeof name === 'string' && typeof description === 'string', 'Invalid arguments for updateCourse');
+    let c = await focusa;
+
+    return await c.courses.findOne(id).exec()
+    .then(async doc =>{
+
+        if(doc) return await doc.atomicPatch({
+            name, description
+        });
+
+        else throw CourseNonExsistent;
+    });
+
+}
+
+const deleteCourse = async (id) =>{
+    assert(typeof id === 'string');
+    let c = await focusa;
+
+    return await c.courses.findOne(id).exec()
+    .then(async doc=>{
+        
+        if(doc){
+            doc.remove();
+            return doc;
+        }
+
+        else throw CourseNonExsistent;
+    });
+}
+
 module.exports = {
-    addCourse, 
+    addCourse, getCourseById,getCourseByName,updateCourse,
 };
