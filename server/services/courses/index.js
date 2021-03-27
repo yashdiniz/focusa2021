2f5407b9f3e7db41f97c516bfd671d7ab4a1afc8
Alston Dias <alston.dias1423@gmail.com>
2021-03-27 12:02:55 +0530

    Pushing errors service auth of courses.

--- server/services/courses/index.js
+++ server/services/courses/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
-const { coursesPort } = require('../../config');
+const { coursesPort, serviceAudience, serviceAuthPass, JWTsignOptions, authRealm } = require('../../config');
 const { getCourseById, getCourseByName, addCourse, updateCourse, deleteCourse } = require('./functions');
 const jwt = require('../jwt');
+
+const { create } = require('axios');
+let token = '';
+const auth = create({
+    baseURL: `${authRealm}`,
+    timeout: 5000,
+});
+let loginDetails = Buffer.from(`courses:${serviceAuthPass}`).toString('base64');
+auth.get('/', {
+    headers: `Basic ${loginDetails}`
+}).then(res => token = res.data.token);
+setInterval(() => auth.get('/', {
+    headers: `Basic ${loginDetails}`
+}).then(res => token = res.data.token), (JWTsignOptions.expiresIn-10)*1000);
+
 
 app.get('/getCourseById', jwt.ensureLoggedIn, (req, res) => {
     if (req.user) getCourseById(req.query.id)
@@ -23,25 +38,29 @@ app.get('/getCourseByName', jwt.ensureLoggedIn, (req, res) => {
 });
 
 
-app.get('/addCourse', jwt.ensureLoggedIn, (req, res) => {
-
+app.get('/addCourse', jwt.ensureLoggedIn, async (req, res) => {
     if (req.user?.aud === serviceAudience
-        && getRolesOfUser(req.user?.name).find(doc => doc.name === 'admin')) {
-
+        ^ await auth.get('/getRolesOfUser', {
+            params: { name: req.user?.name },
+            headers: { authorization: token },
+        }).then(res => res.data.find(doc => doc.name === 'admin'))) {
         addCourse(req.query.name, req.query.description)
             .then(doc => {
                 res.json({ name: doc.name, uuid: doc.uuid });
             }).catch(e => {
-                res.status(404).json({ message: 'course exists', e })
+                res.status(404).json({ e })
             })
     }
 
     else res.status(403).json({ message: 'Operation not allowed.' });
 });
 
-app.get('/updateCourse', jwt.ensureLoggedIn, (req, res) => {
+app.get('/updateCourse', jwt.ensureLoggedIn, async (req, res) => {
     if (req.user?.aud === serviceAudience
-        && getRolesOfUser(req.user?.name).find(doc => doc.name === 'admin')) {
+        ^ await auth.get('/getRolesOfUser', {
+            params: { name: req.user?.name },
+            headers: { authorization: token },
+        }).then(res => res.data.find(doc => doc.name === 'admin'))) {
             updateCourse(req.query.id, req.query.name, req.query.description)
             .then(doc => {
                 res.json({ name: doc.name, uuid: doc.uuid });
@@ -53,9 +72,12 @@ app.get('/updateCourse', jwt.ensureLoggedIn, (req, res) => {
     
 });
 
-app.get('/deleteCourse', jwt.ensureLoggedIn, (req, res) => {
+app.get('/deleteCourse', jwt.ensureLoggedIn, async (req, res) => {
     if (req.user?.aud === serviceAudience
-        && getRolesOfUser(req.user?.name).find(doc => doc.name === 'admin')) {
+        ^ await auth.get('/getRolesOfUser', {
+            params: { name: req.user?.name },
+            headers: { authorization: token },
+        }).then(res => res.data.find(doc => doc.name === 'admin'))) {
             deleteCourse(req.query.id)
             .then(doc => {
                 res.json({ name: doc.name, uuid: doc.uuid });
