dc41e890992e040b7103f341a6bfc04fd6b3ba13
Pranav Paranjape <paranjape968@gmail.com>
2021-03-28 22:22:23 +0530

    added response object to getpostbyid in index.js

--- server/services/posts/index.js
+++ server/services/posts/index.js
@@ -6,7 +6,7 @@ const jwt = require('../jwt');
 app.get('/getPostById', jwt.ensureLoggedIn, (req , res)=>{
     if(req.user) getPostById(req.query.id)
     .then(doc=>{
-        res.json({});
+        res.json({uuid: doc.uuid, parent: doc.parent, text: doc.text, course: doc.course, author: doc.author, reported: doc.reported, approved: doc.approved, time: doc.time, attachmentURL: doc.attachmentURL});
     }).catch(e=>{
         res.status(404).json({message: 'Post not found.',e});
     })
