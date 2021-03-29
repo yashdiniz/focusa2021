11c8d73a385f547f19e51b640c21d7831b4b7689
Yash Diniz <yashdiniz@gmail.com>
2021-03-29 18:57:33 +0530

    Added service auth, and also update and delete.

--- server/services/profile/index.js
+++ server/services/profile/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
-const { profilePort, secret, JWTsignOptions, serviceAuthPass, serviceAudience } = require('../../config');
-const { getProfile } = require('./functions');
+const { profilePort, JWTsignOptions, serviceAuthPass, serviceAudience, authRealm } = require('../../config');
+const { getProfile, updateProfile, deleteProfile } = require('./functions');
 const jwt = require('../jwt');
+
+const { create } = require('axios');
+let token = '';
+const auth = create({
+    baseURL: `${authRealm}`,
+    timeout: 5000,
+});
+let loginDetails = Buffer.from(`profile:${serviceAuthPass}`).toString('base64');
+auth.get('/', {
+    headers: {authorization: `Basic ${loginDetails}`}
+}).then(res => token = res.data.token);
+setInterval(() => auth.get('/', {
+    headers: {authorization:`Basic ${loginDetails}`}
+}).then(res => token = res.data.token), (JWTsignOptions.expiresIn-10)*1000);
 
 app.get('/getProfile', jwt.ensureLoggedIn, (req, res) => {
     if(req.user) getProfile(req.query.id)
@@ -13,6 +27,42 @@ app.get('/getProfile', jwt.ensureLoggedIn, (req, res) => {
     })).catch(e => {
         res.status(404).json({ message: 'Profile not found.', e });
     });
+});
+
+app.get('/updateProfile', jwt.ensureLoggedIn, async (req, res) => {
+    if (req.user?.aud === serviceAudience
+        ^ typeof await auth.get('/userHasRole', {
+            params: { user: req.user?.name, role: 'admin' },
+            headers: { authorization: token },
+        }).then(r => r.data).catch(e => undefined) !== 'undefined') {
+        updateProfile(req.query.id, req.query.fullName, req.query.about, req.query.display_pic)
+        .then(doc => res.json({ userID: doc.userID, 
+            fullName: doc.fullName, 
+            about: doc.about, 
+            interests: doc.interests 
+        })).catch(e => {
+            res.status(404).json({ message: 'Profile not found.', e });
+        });
+            
+    } else res.status(403).json({ message: 'Operation not allowed.' });
+});
+
+
+app.get('/deleteProfile', jwt.ensureLoggedIn, async (req, res) => {
+    if (req.user?.aud === serviceAudience
+        ^ typeof await auth.get('/userHasRole', {
+            params: { user: req.user?.name, role: 'admin' },
+            headers: { authorization: token },
+        }).then(r => r.data).catch(e => undefined) !== 'undefined') {
+        deleteProfile(req.query.id)
+        .then(doc => res.json({ userID: doc.userID, 
+            fullName: doc.fullName, 
+            about: doc.about, 
+            interests: doc.interests 
+        })).catch(e => {
+            res.status(404).json({ message: 'Profile not found.', e })
+        });
+    } else res.status(403).json({ message: 'Operation not allowed.' });
 });
 
 app.listen(profilePort, () => {
