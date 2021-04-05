d5d38372b01f2381631847982a8f2844d3bca23b
Yash Diniz <yashdiniz@gmail.com>
2021-04-05 15:05:46 +0530

    Removed that accidental console.log

--- server/services/posts/functions.js
+++ server/services/posts/functions.js
@@ -163,4 +163,3 @@ const getPostsByCourse = async (courseID) => {
 }
 
 module.exports = {getPostByID, deletePost, createPost, editPost, searchPosts, getPostsByAuthor, getPostsByCourse};
-console.log('Request IP, Payload IP, Proxied IP: ', req.ip, payload.ip, req.headers.realip);
