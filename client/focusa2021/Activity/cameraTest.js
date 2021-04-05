6e266011f7d7b26b62200d22a5ca8dfb01ed33f7
Yash Diniz <yashdiniz@gmail.com>
2021-04-05 20:48:09 +0530

    Attempting to remove need of simple-peer

--- client/focusa2021/Activity/cameraTest.js
+++ client/focusa2021/Activity/cameraTest.js
@@ -1,41 +1,57 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { mediaDevices } from 'react-native-webrtc';
+import { RTCView, mediaDevices, RTCPeerConnection } from 'react-native-webrtc';
 import io from 'socket.io-client';
 import Peer from 'simple-peer';
-import Video from 'react-native-video';
-import { checkDocument } from '@apollo/client/utilities';
-import {Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import styles from '../Styles/CameraTestStyles';
 
-const socket = io.connect("http://192.168.43.71:5000");
+const socket = io.connect("http://192.168.44.72:5000");
 
-const CameraTest = () => {
+const CameraTest = ({ navigation, route, token }) => {
     const [stream, setStream]= useState();
+    const [myStream, setMyStream] = useState();
     const [ID, setID] = useState();
     const [receiver, setReceiver] = useState();
     const [name, setName] = useState();
     const [signal, setCallerSignal] = useState();
     const [callAccepted, setCallAccepted] = useState();
     const [callEnded, setCallEnded] = useState();
-    const myVideo = useRef();
-    const userVideo = useRef();
     const connectionRef = useRef();
-    // const configuration = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
-//  const pc = new RTCPeerConnection(configuration);
 
+    const configuration = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
 
     let isFront = true;
     useEffect(() => {
-        mediaDevices.enumerateDevices().then(sourceInfos => {
-            console.log(sourceInfos);
+        mediaDevices.getUserMedia({
+            audio: true,
+            video: {
+                width: 640,
+                height: 480,
+                frameRate: 30,
+                facingMode: (isFront ? "user" : "environment"),
+                deviceId: undefined
+            }
+        })
+        .then(() => mediaDevices.enumerateDevices())
+        .then(sourceInfos => {
+            console.log('Enumerate devices:', sourceInfos);
             let videoSourceId;
             for (let i = 0; i < sourceInfos.length; i++) {
             const sourceInfo = sourceInfos[i];
-            if(sourceInfo.kind == "videoinput" && sourceInfo.facing == (isFront ? "front" : "environment")) {
-                videoSourceId = sourceInfo.deviceId;
+                if(sourceInfo.kind == "videoinput" && sourceInfo.facing == (isFront ? "front" : "environment")) {
+                    videoSourceId = sourceInfo.deviceId;
+                }
             }
-            }
-            mediaDevices.getUserMedia({
+
+            socket.on('me', id => setID(id));
+            socket.on('callUser', data => {
+                setReceiver(true);
+                setCaller(data.from);
+                setName(data.name);
+                setCallerSignal(data.signal);
+            });
+
+            return mediaDevices.getUserMedia({
                 audio: true,
                 video: {
                     width: 640,
@@ -44,26 +60,31 @@ const CameraTest = () => {
                     facingMode: (isFront ? "user" : "environment"),
                     deviceId: videoSourceId
                 }
-            }).then(stream => {
-                setStream(stream);
-                myVideo.current.srcObject = stream;
-            }).catch(console.error);
-            socket.on('me', id => setID(id));
-            socket.on('callUser', data => {
-                setReceiver(true);
-                setCaller(data.from);
-                setName(data.name);
-                setCallerSignal(data.signal);
-            });
-        });      
-    }, []);
+            })
+            .then(setMyStream)
+            .catch(console.error);
+        }).catch(console.error);      
+    });
 
-    const callUser = (id) => {
-        const peer = new Peer({
-            initiator: true,
-            trickle: false,
-            stream,
+    const callUser = (id) => () => {
+        const pc = new RTCPeerConnection(configuration);
+        // const peer = new Peer({
+        //     initiator: true,
+        //     wrtc: WebRTC
+        // });
+
+        pc.createOffer().then(desc => {
+            pc.setLocalDescription(desc).then(() => {
+                // Send pc.localDescription to peer
+                console.log(pc.localDescription)
+            });
         });
+        
+        pc.onicecandidate = function (event) {
+            // send event.candidate to peer
+            console.log(event)
+        };
+
         peer.on('signal', data => {
             socket.emit('callUser', {
                 userToCall: id,
@@ -72,14 +93,20 @@ const CameraTest = () => {
                 name,
             });
         });
-        peer.on('stream', stream => {
-            userVideo.current.srcObject = stream;
+
+        pc.onaddstream(stream => {
+            setStream(stream);
         });
+        
+        // peer.on('stream', stream => {
+        //     setStream(stream);
+        // });
+        
         socket.on('callAccepted', signal => {
             setCallAccepted(true);
             peer.signal(signal);
         });
-        connectionRef.current = peer;
+        connectionRef.current = pc;
         return 0;
     }
 
@@ -96,7 +123,7 @@ const CameraTest = () => {
         })
 
         peer.on("stream", (stream)=>{
-            userVideo.current.srcObject = stream
+            setStream(stream);
         })
 
         peer.signal(callerSignal)
@@ -112,16 +139,16 @@ const CameraTest = () => {
 
     return (
         <View>
-            { stream && <Video muted ref={myVideo} autoPlay style={{width:'300px'}}/>}
+            { stream && <RTCView streamURL={myStream.toURL()}/>}
             { callAccepted && !callEnded ? 
-            <Video muted ref={userVideo} autoPlay style={{width:'300px'}}/>:
+            <RTCView streamURL={stream.toURL()}/>:
             null
             }
             
-            <TouchableOpacity style={styles.MakeCall} onPress={callUser(ID)}>
+            <TouchableOpacity style={styles.MakeCall} onPress={callUser()}>
                 <Text>Call</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.MakeCall} onPress={answerCall()}>
+            <TouchableOpacity style={styles.MakeCall} onPress={answerCall}>
                 <Text>Answer Call</Text>
             </TouchableOpacity>
         </View>
