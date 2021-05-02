b2e76d815562ff5a1ececd4cc9d5bc7de0cbf67e
Alston Dias <alston.dias1423@gmail.com>
2021-05-02 09:46:13 +0530

    completexd the profile page UI

--- client/focusa2021/Activity/Profile.js
+++ client/focusa2021/Activity/Profile.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import {View,Text,StatusBar} from 'react-native';
+import {View,Text,StatusBar,Dimensions} from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import styles from '../Styles/ProfileStyles';
 
 import {ensureAuthenticated} from './ensureAuthenticated';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const Profile = ({ navigation, route, token }) =>{
     ensureAuthenticated(navigation, token);
     return(
-        <View style={{alignItems:'center', justifyContent:'center'}}>
+        <View style={styles.container}>
             <StatusBar backgroundColor = "#ffffff" barStyle="dark-content"/>
-            <Text style={{fontSize: 20}}>This is the Profiles page</Text>
+
+            <TouchableOpacity style={{paddingRight: 25, width: Dimensions.get('screen').width, alignItems:'flex-end'}}>
+                    <MaterialCommunityIcons name="cog" size={35} style={{marginTop: 10,}} />
+            </TouchableOpacity>
+
+            <View style={{alignItems:'center',justifyContent:'center',width:Dimensions.get('screen').width, height:Dimensions.get('screen').height-100}}>
+                <MaterialCommunityIcons name="account" size={80} style={styles.UserIcon} />
+                <Text style={styles.nameText}>Alston Dias</Text>
+                <Text style={styles.usernameText}>@alstar_1402</Text>
+                <Text style={styles.classText}>B.E Computer</Text>
+                <Text style={styles.collegeNameText}>Don Bosco College of Engineering</Text>
+                <View style={{ borderColor: 'black',borderBottomWidth: 1, marginTop: 200, width:Dimensions.get('screen').width}}/>
+
+                <View>
+                    <TouchableOpacity style={{flexDirection:'row'}}>
+                        <Text style={{fontSize:20, marginTop:20}}>Courses Following</Text>
+                        <MaterialCommunityIcons name="greater-than" size={35} style={{marginTop: 19,}} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+        
+            
+
+           
+
         </View>
     )
 }
