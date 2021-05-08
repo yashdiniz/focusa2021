f15c09a041f7c77ce1fbcd2553e36b6e8e7a2fcc
Alston Dias <alston.dias1423@gmail.com>
2021-05-08 11:12:03 +0530

    Created settings component.

--- client/focusa2021/Activity/Setting.js
+++ client/focusa2021/Activity/Setting.js
@@ -4,7 +4,12 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import styles from '../Styles/SettingStyle';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const Setting =({navigation}) =>{
+const Setting =({navigation, token}) =>{
+
+    function onPressLogout(){
+            token = !token;
+            ()=>navigation.naviagte('Profile')
+    }
     return(
         <View style={styles.container}>
            <StatusBar backgroundColor="#ffffff" barStyle="dark-content" />
@@ -12,8 +17,8 @@ const Setting =({navigation}) =>{
                 <MaterialCommunityIcons name="arrow-left" size={25} style={{marginTop: 19,}} />
            </TouchableOpacity>
            <View style={{alignItems:'center',backgroundColor:'white', position:'absolute', bottom:0, marginBottom: 20}}>
-           <TouchableOpacity style={styles.LoginOutButton}>
-               <Text style={{color:'white'}}>Login</Text>
+           <TouchableOpacity style={styles.LoginOutButton} onPress={onPressLogout}>
+               <Text style={{color:'white'}}>Logout</Text>
            </TouchableOpacity>
            </View>
         </View>
