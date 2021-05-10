de3cd64e239972f7f05e3678d50d9f2f6fc5ee56
Alston Dias <alston.dias1423@gmail.com>
2021-05-10 11:18:48 +0530

    added the login part to the courses abd video activity

--- client/focusa2021/Activity/courses.js
+++ client/focusa2021/Activity/courses.js
@@ -1,27 +1,39 @@
-import React from 'react';
-import { StatusBar,ScrollView, TouchableOpacity } from 'react-native';
+import React,{useState,useCallback} from 'react';
+import { StatusBar,ScrollView, TouchableOpacity, RefreshControl } from 'react-native';
 import SearchBar from '../Components/SearchBar';
 import styles from '../Styles/CourseStyles';
 import Course from '../Components/Course';
-
+import ErrorLogin from '../Components/ErrorLogin'
 import { ensureAuthenticated } from '../interface/ensureAuthenticated';
 
 const Courses = ({ navigation, route, token }) =>{
+
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        wait(2000).then(() => setRefreshing(false));
+    }, []);
+
     ensureAuthenticated(navigation, token);
     return(
-        <ScrollView contentContainerStyle={styles.CoursesView}>
+
+        <ScrollView contentContainerStyle={styles.CoursesView}
+        refreshControl={
+            <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            />
+        }>
             <StatusBar backgroundColor = "#ffffff" barStyle="dark-content"/>
             <SearchBar/>
 
+            {token ?(
             <TouchableOpacity onPress={()=>navigation.navigate('SubjectPage')}>
                 <Course/>
-            </TouchableOpacity>
-            
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
+            </TouchableOpacity>):(<ErrorLogin navigation={navigation}/>)}
+
+
         </ScrollView>
     )
 }
