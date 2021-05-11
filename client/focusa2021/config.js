6991dbbea423e02135c99bd7e95b5ff58adce4cf
Alston Dias <alston.dias1423@gmail.com>
2021-05-11 23:02:40 +0530

    changed ip to mac book

--- client/focusa2021/config.js
+++ client/focusa2021/config.js
@@ -1,2 +1,2 @@
-export const graphQLRealm = 'http://192.168.43.71:1896';
-export const authRealm = 'http://192.168.43.71:1897';
+export const graphQLRealm = 'http://192.168.0.102:1896';
+export const authRealm = 'http://192.168.0.102:1897';
