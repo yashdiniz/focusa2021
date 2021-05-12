6cde795a758b3c94ecbeff46697e9816fc292f23
Yash Diniz <yashdiniz@gmail.com>
2021-05-12 11:36:13 +0530

    updated IP.

--- client/focusa2021/config.js
+++ client/focusa2021/config.js
@@ -1,2 +1,2 @@
-export const graphQLRealm = 'http://192.168.43.71:1896';
-export const authRealm = 'http://192.168.43.71:1897';
+export const graphQLRealm = 'http://192.168.42.74:1896';
+export const authRealm = 'http://192.168.42.74:1897';
