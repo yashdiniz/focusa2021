02f36a816a85cceaf54942e8fc1c29eab9ff3f90
Yash Diniz <yashdiniz@gmail.com>
2021-05-12 11:30:51 +0530

    Updated query to match the schema.

--- client/focusa2021/queries.js
+++ client/focusa2021/queries.js
@@ -1,11 +1,11 @@
 import { gql } from '@apollo/client';
 
 export const getProfileData = gql`
-    query getProfileData($username: ID!) {
+    query getProfileData($username: String!) {
         user(name: $username) {
             name,
             profile{
-                fullName, about
+                fullName
             }
         }
     }
