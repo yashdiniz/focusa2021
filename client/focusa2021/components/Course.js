6b35da69ca857360b8d8dce8b7bf5cab956b16dc
Yash Diniz <yashdiniz@gmail.com>
2021-05-17 15:06:20 +0530

    Add Course Component.

--- client/focusa2021/components/Course.js
+++ client/focusa2021/components/Course.js
@@ -1,3 +1,32 @@
-function Course() {
-    
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Card, Text } from 'react-native-elements';
+
+function Course({ name, description }) {
+    return (
+        <Card>
+            <Text 
+                h1
+                style={styles.text}
+            >
+                {name}
+            </Text>
+            <Text
+                style={{
+                    ...styles.text,
+                    marginBottom: 20,
+                }}
+            >
+                {description}
+            </Text>
+        </Card>
+    );
 }
+
+const styles = StyleSheet.create({
+    text: {
+        marginLeft: 20,
+    }
+});
+
+export default Course;
