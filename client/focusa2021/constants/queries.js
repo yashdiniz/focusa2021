e19dc671ad36a6bb9967e7e4faccf00302631b1a
Yash Diniz <yashdiniz@gmail.com>
2021-05-18 11:33:48 +0530

    Added query to getCourseDetails.

--- client/focusa2021/constants/queries.js
+++ client/focusa2021/constants/queries.js
@@ -3,6 +3,7 @@ import { gql } from '@apollo/client';
 export const getProfileData = gql`
     query getProfileData($username: String!) {
         user(name: $username) {
+            uuid,
             name,
             profile{
                 fullName, about
@@ -14,9 +15,28 @@ export const getProfileData = gql`
 export const getCourses = gql`
     query getProfileData($username: String!) {
         user(name: $username) {
+            uuid,
             profile{
+                uuid,
                 interests{
-                    name, description
+                    uuid, name, description
+                }
+            }
+        }
+    }
+`;
+
+export const getCourseDetails = gql`
+    query getCourseDetails($courseID: ID!) {
+        course(id: $courseID) {
+            uuid, name, description,
+            posts {
+                uuid, text, attachmentURL, time,
+                author {
+                    uuid, name
+                },
+                course {
+                    uuid, name
                 }
             }
         }
