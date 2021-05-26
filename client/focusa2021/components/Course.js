4253b65bb501f452de4c80c9e9366288d9ab54d6
Alston Dias <alston.dias1423@gmail.com>
2021-05-26 20:09:41 +0530

    added the customised card view from old code

--- client/focusa2021/components/Course.js
+++ client/focusa2021/components/Course.js
@@ -18,12 +18,42 @@ function Course({ name, description }) {
                 {description}
             </Text>
         </Card>
+
+        // <View style={styles.singleCourseView}>
+        //     <Text style={styles.SubjectTitle}>
+        //         Compiler Constructions
+        //     </Text>
+        //     <Text style={styles.SubjectDescription}>
+        //         All notes and Videos pertaining to Compiler Constructions will be posted here.
+        //     </Text>
+        // </View>
     );
 }
 
 const styles = StyleSheet.create({
     text: {
         marginBottom: 20,
+    },
+    singleCourseView:{
+        borderColor: 'grey',
+        borderWidth: 1,
+        width: Dimensions.get('screen').width -15,
+        height: 130,
+        marginTop: 10,
+        backgroundColor: '#ffffff',
+        borderRadius: 16,
+
+    },
+    SubjectTitle:{
+        marginTop: 10,
+        paddingLeft: 10,
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    SubjectDescription:{
+        paddingLeft: 20,
+        marginTop: 10,
+        color: 'grey'
     }
 });
 
