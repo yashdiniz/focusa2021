37861c30d458ae80fe406c1776fa5687e97078e6
Yash Diniz <yashdiniz@gmail.com>
2021-05-27 22:07:45 +0530

    Added SET_USERID to redux store.

--- client/focusa2021/config.js
+++ client/focusa2021/config.js
@@ -4,6 +4,7 @@ export const authRealm = 'http://192.168.0.104:1897';
 // Exporting the REDUX action types from here.
 export const SET_TOKEN = 'SET_TOKEN';
 export const SET_USERNAME = 'SET_USERNAME';
+export const SET_USERID = 'SET_USERID';
 
 // document render limit, to improve performance
 export const documentLimit = 25;
