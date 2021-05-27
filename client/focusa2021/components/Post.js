648cfb0bb8473dfbe96bbaab664a9a462af0b929
Yash Diniz <yashdiniz@gmail.com>
2021-05-27 12:23:53 +0530

    console.log for both string date formats. Also added reference for custom Date object.

--- client/focusa2021/components/Post.js
+++ client/focusa2021/components/Post.js
@@ -5,6 +5,11 @@ import { Card, Text } from 'react-native-elements';
 function Post(props/*{ author, course, time, text, attachmentURL }*/) {
     // TODO: make provisions for parent and comments!
 
+    // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+    const date = new Date(time);
+    console.log('Date in Post', date.toUTCString(), date.toISOString());
+    console.log(`Custom Date string: ${date.getFullYear()}-${date.getMonth()}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`)
+
     return (
         <Card>
             <Text>
