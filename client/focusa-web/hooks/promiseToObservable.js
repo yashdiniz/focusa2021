8bd76d146a6d3ad1fe401950a6f55cc97f7a5b08
Yash Diniz <yashdiniz@gmail.com>
2021-06-07 11:30:28 +0530

    Added Apollo client configuration.

---
+++ client/focusa-web/hooks/promiseToObservable.js
@@ -0,0 +1,17 @@
+/**
+ *  https://github.com/apollographql/apollo-link/issues/646#issuecomment-423279220
+ */
+import { Observable } from 'apollo-link';
+
+export default promise =>
+    new Observable((subscriber) => {
+        promise.then(
+            (value) => {
+                if (subscriber.closed) return;
+                subscriber.next(value);
+                subscriber.complete();
+            },
+            err => subscriber.error(err)
+        );
+        return subscriber; // this line can removed
+    });
