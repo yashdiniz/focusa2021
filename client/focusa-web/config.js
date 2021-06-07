127aa8307016cb9a501680d449ccc8221892796e
Yash Diniz <yashdiniz@gmail.com>
2021-06-07 11:29:38 +0530

    Add Authentication related functions. Added axios.

---
+++ client/focusa-web/config.js
@@ -0,0 +1,7 @@
+export const graphQLRealm = 'http://192.168.42.130:1896';
+export const authRealm = 'http://192.168.42.130:1897';
+
+// Exporting the REDUX action types from here.
+export const SET_TOKEN = 'SET_TOKEN';
+export const SET_USERNAME = 'SET_USERNAME';
+export const SET_USERID = 'SET_USERID';
---
+++ client/focusa-web/hooks/authenticate.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react';
+import { SET_TOKEN, SET_USERNAME, SET_USERID } from '../config';
+import { store } from './store';
+import { create } from 'axios';
+
+export const auth = create({
+    baseURL: authRealm,
+    withCredentials: true, // enable use of cookies outside web browser
+});
+
+/**
+ * 
+ * @param {string} username The username to login with.
+ * @param {string} password The password to login with.
+ */
+export const authenticate = (username, password) => {
+    const [token, setToken] = useState('');
+
+    const params = new URLSearchParams();
+    params.append('username', username);
+    params.append('password', password);
+
+    useEffect(() => {
+        auth.post('/login', { params })
+            .then(res => {
+                setToken(res.data.token);
+                store.dispatch({ type: SET_USERID, userID: res.data.uuid });
+                store.dispatch({ type: SET_TOKEN, token: res.data.token });
+                store.dispatch({ type: SET_USERNAME, username: res.data.name });
+                return token;
+            })
+            .catch(e => console.error(new Date(), 'authenticate Error', e));
+    }, []);
+
+    return token;
+}
+
+/**
+ * 
+ * @returns Promise with refreshed token.
+ */
+ export const refresh = () => {
+    return auth.get('/refresh')
+        .then(res => store.dispatch({ type: SET_TOKEN, token: res.data.token }))
+        .catch(e => console.error(new Date(), 'refresh', e));
+}
+
+/**
+ * Logout is used to invalidate the refresh token offered by the server,
+ * and update the token currently in memory.
+ * @returns Promise.
+ */
+ export const logout = () => {
+    return auth.get('/logout')
+        .then(() => {
+            store.dispatch({ type: SET_TOKEN, token: '' });
+            store.dispatch({ type: SET_USERID, userID: '' });
+            store.dispatch({ type: SET_USERNAME, username: '@@' });
+            BackHandler.exitApp();
+        })
+        .catch(e => console.error(new Date(), 'logout', e));
+}
